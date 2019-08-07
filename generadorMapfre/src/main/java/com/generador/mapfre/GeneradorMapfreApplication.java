6ab634dea4c2611d9a986a32142f229f0dffe4c4
Alberto Windows <albertogonben@gmail.com>
2019-08-07 08:32:56 +0200

    Actualizacion

--- generadorMapfre/src/main/java/com/generador/mapfre/GeneradorMapfreApplication.java
+++ generadorMapfre/src/main/java/com/generador/mapfre/GeneradorMapfreApplication.java
@@ -1,16 +1,19 @@
 package com.generador.mapfre;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GeneradorMapfreApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(GeneradorMapfreApplication.class, args);
+		SpringApplication.run(GeneradorMapfreApplication.class, args);
 
 		GeneradorJFace.start();
 //        GeneradorMainJava.start();
 
 	}
+
+	
 
 }
